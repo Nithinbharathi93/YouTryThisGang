1c295f3c87881d409722b740c0b4d2c51dc3b402
Nithinbharathi <106980567+Nithinbharathi93@users.noreply.github.com>
2023-09-26 18:27:03 +0530

    new file created

---
+++ Frontend_dev/nithin/justJS/code.js
@@ -0,0 +1,21 @@
+let person1 = "nithin";
+const person2 = "deepa";
+var years = 1;
+
+function runTime(yr) {
+    year++;
+}
+
+const person3 = {
+    name:"Deepali", 
+    relation:"sister", 
+    age:19
+};
+
+const person4 = {
+    name:"sabarish",
+    relation:"friend",
+    age:19
+}
+
+nm = person3.name;
